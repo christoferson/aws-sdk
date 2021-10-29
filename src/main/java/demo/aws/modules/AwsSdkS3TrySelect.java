f0a677e8a017977a6d78cdc954f8a4efc56ea6e9
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-10-30 01:21:57 +0900

    Add AwsSdkS3TrySelect

---
+++ src/main/java/demo/aws/modules/AwsSdkS3TrySelect.java
@@ -0,0 +1,103 @@
+package demo.aws.modules;
+
+import static com.amazonaws.util.IOUtils.copy;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CSVInput;
+import com.amazonaws.services.s3.model.CSVOutput;
+import com.amazonaws.services.s3.model.CompressionType;
+import com.amazonaws.services.s3.model.ExpressionType;
+import com.amazonaws.services.s3.model.InputSerialization;
+import com.amazonaws.services.s3.model.OutputSerialization;
+import com.amazonaws.services.s3.model.SelectObjectContentEvent;
+import com.amazonaws.services.s3.model.SelectObjectContentEventVisitor;
+import com.amazonaws.services.s3.model.SelectObjectContentRequest;
+import com.amazonaws.services.s3.model.SelectObjectContentResult;
+
+public class AwsSdkS3TrySelect {
+	
+	private AmazonS3 s3client;
+	
+	public AwsSdkS3TrySelect(AWSCredentials credentials) {
+		
+		s3client = AmazonS3ClientBuilder
+				  .standard()
+				  .withCredentials(new AWSStaticCredentialsProvider(credentials))
+				  .withRegion(Regions.US_EAST_1)
+				  .build();
+	}
+	
+
+    private static final String S3_SELECT_RESULTS_PATH = "s3-results.txt";
+    private static final String QUERY = "select s._1, s._2 from S3Object s";
+
+    public void select(String bucket, String key) throws Exception {
+
+        SelectObjectContentRequest request = generateBaseCSVRequest(bucket, key, QUERY);
+        final AtomicBoolean isResultComplete = new AtomicBoolean(false);
+
+        try (OutputStream fileOutputStream = new FileOutputStream(new File (S3_SELECT_RESULTS_PATH));
+             SelectObjectContentResult result = s3client.selectObjectContent(request)) {
+            InputStream resultInputStream = result.getPayload().getRecordsInputStream(
+                    new SelectObjectContentEventVisitor() {
+                        @Override
+                        public void visit(SelectObjectContentEvent.StatsEvent event) {
+                            System.out.println(
+                                    "Received Stats, Bytes Scanned: " + event.getDetails().getBytesScanned()
+                                            +  " Bytes Processed: " + event.getDetails().getBytesProcessed());
+                        }
+
+                        /*
+                         * An End Event informs that the request has finished successfully.
+                         */
+                        @Override
+                        public void visit(SelectObjectContentEvent.EndEvent event) {
+                            isResultComplete.set(true);
+                            System.out.println("Received End Event. Result is complete.");
+                        }
+                    }
+            );
+
+            copy(resultInputStream, fileOutputStream);
+        }
+
+        /*
+         * The End Event indicates all matching records have been transmitted.
+         * If the End Event is not received, the results may be incomplete.
+         */
+        if (!isResultComplete.get()) {
+            throw new Exception("S3 Select request was incomplete as End Event was not received.");
+        }
+    }
+
+    private static SelectObjectContentRequest generateBaseCSVRequest(String bucket, String key, String query) {
+        SelectObjectContentRequest request = new SelectObjectContentRequest();
+        request.setBucketName(bucket);
+        request.setKey(key);
+        request.setExpression(query);
+        request.setExpressionType(ExpressionType.SQL);
+
+        // Csv Input with no compression
+        InputSerialization inputSerialization = new InputSerialization();
+        inputSerialization.setCsv(new CSVInput());
+        inputSerialization.setCompressionType(CompressionType.NONE);
+        request.setInputSerialization(inputSerialization);
+
+        OutputSerialization outputSerialization = new OutputSerialization();
+        outputSerialization.setCsv(new CSVOutput());
+        request.setOutputSerialization(outputSerialization);
+
+        return request;
+    }
+	
+}
