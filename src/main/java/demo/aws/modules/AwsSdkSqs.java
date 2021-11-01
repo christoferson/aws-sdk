61504f94042791196f21464bc416e1fe265239e0
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-11-01 16:53:54 +0900

    Add SQS queueList

--- src/main/java/demo/aws/modules/AwsSdkSqs.java
+++ src/main/java/demo/aws/modules/AwsSdkSqs.java
@@ -5,6 +5,7 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.ListQueuesResult;
 
 public class AwsSdkSqs {
 	
@@ -17,6 +18,16 @@ public class AwsSdkSqs {
 				  .withCredentials(new AWSStaticCredentialsProvider(credentials))
 				  .withRegion((region == null)? Regions.US_EAST_1 : region)
 				  .build();
+	}
+
+	public void queueList() {
+		
+		ListQueuesResult result = client.listQueues();
+		System.out.println("Your SQS Queue URLs:");
+		for (String url : result.getQueueUrls()) {
+		    System.out.println(url);
+		}
+
 	}
 	
 }
