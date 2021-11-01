ae685c09a0fd407370316f39b8836d11851320be
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-11-02 01:08:54 +0900

    SQS Add Message Receive

--- src/main/java/demo/aws/modules/AwsSdkSqs.java
+++ src/main/java/demo/aws/modules/AwsSdkSqs.java
@@ -1,11 +1,16 @@
 package demo.aws.modules;
 
+import java.util.List;
+
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.ListQueuesResult;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
 
 public class AwsSdkSqs {
 	
@@ -35,4 +40,18 @@ public class AwsSdkSqs {
 		String queue_url = client.getQueueUrl(name).getQueueUrl();
 		System.out.println(String.format("Queue:%s URL:%s", name, queue_url));
 	}
+	
+	public void messageReceive(String queueUrl) {
+		
+		System.out.println(String.format("Queue:%s Receiving Message...", queueUrl));
+		
+		ReceiveMessageRequest request = new ReceiveMessageRequest()
+				.withQueueUrl(queueUrl);
+				//.withWaitTimeSeconds(5)
+		List<Message> messages = client.receiveMessage(request).getMessages();
+		
+		System.out.println(String.format("Queue:%s Received Message: %s", queueUrl, messages));
+	}
+	
+
 }
